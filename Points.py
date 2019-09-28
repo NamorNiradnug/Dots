8c77bee44ff001cbe6a81bfe1370bb09f1ac687f
NamorNiradnug <50048443+NamorNiradnug@users.noreply.github.com>
2019-09-28 16:06:26 +0300

    Bug in create_point!!

--- Points.py
+++ Points.py
@@ -1,30 +1,35 @@
 from tkinter import Tk, Canvas
-from points_res import points_map, players_color_list
 
+points_map = [[0] * 100 for __ in range(100)]
+first_player_color, second_player_color = "red", "green"
+players_color_list = [0, first_player_color, second_player_color]
 
 zoom_koef = [2]
+paper_coords = [0, 0]
+zero_move = [0, 0]
 root = Tk()
 paper = Canvas(root, width = 160 * 4, height = 4 * 160)
 paper.pack() 
 
 
 def draw_point(point_type, x, y):
+    paper.create_line((x * 16 + 8) * zoom_koef[0], y * zoom_koef[0] * 16, (x * 16 + 8) * zoom_koef[0], (y + 1) * zoom_koef[0] * 16)
+    paper.create_line(x * zoom_koef[0] * 16, (y * 16 + 8) * zoom_koef[0], (x + 1) * zoom_koef[0] * 16,  (y * 16 + 8) * zoom_koef[0])
     if point_type != 0:
         paper.create_oval((x * 16 + 5) * zoom_koef[0], (y * 16 + 5) * zoom_koef[0], (x * 16 + 11) * zoom_koef[0], (y * 16 + 11) * zoom_koef[0], fill = players_color_list[point_type])
 
 
-def draw_paper():
-    for i in range(1, len(points_map) + 1):
-        paper.create_line(0, (i * 16 - 8) * zoom_koef[0], 16 * len(points_map[0]) * zoom_koef[0], (i * 16 - 8) * zoom_koef[0], width = 1 * zoom_koef[0])
-    for i in range(1, len(points_map[0]) + 1):
-        paper.create_line((i * 16 - 8) * zoom_koef[0], 0, (i * 16 - 8) * zoom_koef[0], 16 * len(points_map) * zoom_koef[0], width = 1 * zoom_koef[0])
-    for i in range(len(points_map)):
-        for j in range(len(points_map[i])):
-            draw_point(points_map[i][j], j, i)
+def draw_paper(x, y):
+    paper_coords[0] = x
+    paper_coords[1] = y
+    w = h =  640 // (zoom_koef[0] * 16)
+    for i in range(len(points_map[y:y + h:])):
+        for j in range(len(points_map[i][x:x + w:])):
+            draw_point(points_map[y:y + h:][i][x:x + w:][j], j, i)
 
 
 def to_zoom_koef(zoom_k):
-    if zoom_k != 0:
+    if zoom_k != 0 and zoom_k != 128:
         paper.delete('all')
         zoom_koef[0] = zoom_k
         draw_paper()    
@@ -32,11 +37,35 @@ def to_zoom_koef(zoom_k):
 
 
 def move_screen(x, y):
-    paper.move("all", -x, -y)
+    if paper_coords[1] + y <= 0:
+        points_map.insert(0, [0] * len(points_map[0]))
+        paper_coords[1] += 1
+        zero_move[1] -= 1
+    if paper_coords[0] + x <= 0:
+        for i in range(len(points_map)):
+            points_map[i].insert(0, 0)
+        paper_coords[0] += 1
+        zero_move[0] += 1
+    paper.delete('all')
+    draw_paper(paper_coords[0] + x, paper_coords[1] + y)
+    
+    
+def create_point(x, y):
+    x ,y = x // (16 * zoom_koef[0]) - paper_coords[0], y // (16 * zoom_koef[0]) - paper_coords[1]
+    #send to server
+    if points_map[y][x] == 0:
+        print(x, y)
+        points_map[y][x] == 1
+        paper.delete('all')
+        draw_paper(paper_coords[0], paper_coords[1])        
     
 
-draw_paper()
-root.bind('<Control-=>', lambda event: to_zoom_koef(zoom_koef[0]+1))
-root.bind('<Up>', lambda event: move_screen(0, -16 * zoom_koef[0]))
-#root.bind('<Control-->', lambda event: to_zoom_koef(zoom_koef[0]-1))
+draw_paper(1, 1)
+#root.bind('<Control-=>', lambda event: to_zoom_koef(zoom_koef[0] ** 2))
+#root.bind('<MouseWheel>', lambda event: to_zoom_koef(zoom_koef[0] // 2))
+root.bind('<Up>', lambda event: move_screen(0, -1))
+root.bind('<Down>', lambda event: move_screen(0, 1))
+root.bind('<Left>', lambda event: move_screen(-1, 0))
+root.bind('<Right>', lambda event: move_screen(1, 0))
+root.bind('<Button-1>', lambda event: create_point(event.x, event.y))
 root.mainloop()
