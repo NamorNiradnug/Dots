6cd21e9bdeae48a173fcb0b531e2bd5ac39af0b4
NamorNiradnug <50048443+NamorNiradnug@users.noreply.github.com>
2019-09-28 16:06:38 +0300

    Delete points_res.py

--- points_res.py
+++
@@ -1,3 +0,0 @@
-points_map = [[0, 0, 1, 2], [1, 2, 1, 2], [0, 0, 0, 0], [0, 0, 0, 0]]
-first_player_color, second_player_color = "red", "green"
-players_color_list = [0, first_player_color, second_player_color]
