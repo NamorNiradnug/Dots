3a0081d9486223c5a9d56d93cdfff83e92d6b118
korochun <gkorochun@gmail.com>
2019-10-02 19:57:31 +0300

    Minor, changed map format a bit

--- client/dots.py
+++ client/dots.py
@@ -3,10 +3,10 @@ from tkinter import Tk, Canvas
 
 class App:
     def __init__(self):
-        self.points_map = []
+        self.map = []
         for i in range(100):
-            self.points_map.append([0] * 100)
-        self.colors = [0, "#20D020", "blue"]
+            self.map.append([-1] * 100)
+        self.colors = ["#20D020", "blue"]
 
         self.is_green_turn = False
         self.scale = 2
@@ -37,7 +37,7 @@ class App:
         for point in ((p1[0], p1[1] - 1), (p1[0] - 1, p1[1] - 1), (p1[0] - 1, p1[1]), (p1[0] + 1, p1[1] - 1),
                       (p1[0] - 1, p1[1] + 1), (p1[0] + 1, p1[1]), (p1[0] + 1, p1[1] + 1), (p1[0], p1[1] + 1)):
             try:
-                if self.points_map[point[1]][point[0]] == self.points_map[p1[1]][p1[0]] and not ((point, p1) in tracks):
+                if self.map[point[1]][point[0]] == self.map[p1[1]][p1[0]] and not ((point, p1) in tracks):
                     print(tracks, (p1, p2))
                     variable = self.do_connect(point, p2, tracks and {(point, p2), (p2, point)}, length + 1)
                     result += variable[0]
@@ -49,29 +49,29 @@ class App:
     def draw(self, point):
         self.position = point
         w = h = 640 // (self.scale * 16)
-        for i in range(len(self.points_map[point[1]:point[1] + h])):
-            for j in range(len(self.points_map[i][point[0]:point[0] + w])):
+        for i in range(len(self.map[point[1]:point[1] + h])):
+            for j in range(len(self.map[i][point[0]:point[0] + w])):
                 self.canvas.create_line((i * 16 + 8) * self.scale, j * self.scale * 16,
                                         (i * 16 + 8) * self.scale, (j + 1) * self.scale * 16)
                 self.canvas.create_line(i * self.scale * 16, (j * 16 + 8) * self.scale,
                                         (i + 1) * self.scale * 16, (j * 16 + 8) * self.scale)
-                if self.points_map[j:j + h][i][i:i + w][j] != 0:
+                if self.map[j:j + h][i][i:i + w][j] != -1:
                     self.canvas.create_oval((i * 16 + 5) * self.scale, (j * 16 + 5) * self.scale,
                                             (i * 16 + 11) * self.scale, (j * 16 + 11) * self.scale,
                                             fill=self.colors[
-                                                self.points_map[point[1]:point[1] + h][i][point[0]:point[0] + w][j]
+                                                self.map[point[1]:point[1] + h][i][point[0]:point[0] + w][j]
                                             ])
-        for i in range(len(self.points_map[point[1]:point[1] + h - 1])):
-            for j in range(len(self.points_map[i][point[0]:point[0] + w - 1])):
-                if self.points_map[i][j] == self.points_map[i][j + 1] > 0:
+        for i in range(len(self.map[point[1]:point[1] + h - 1])):
+            for j in range(len(self.map[i][point[0]:point[0] + w - 1])):
+                if self.map[i][j] == self.map[i][j + 1] != -1:
                     self.canvas.create_line((j * 16 - 8) * self.scale, (i * 16 - 8) * self.scale,
                                             (j * 16 + 8) * self.scale, (i * 16 - 8) * self.scale,
-                                            fill=self.colors[self.points_map[i][j]],
+                                            fill=self.colors[self.map[i][j]],
                                             width=2 * self.scale)
-                if self.points_map[i][j] == self.points_map[i + 1][j] > 0:
+                if self.map[i][j] == self.map[i + 1][j] != -1:
                     self.canvas.create_line((j * 16 - 8) * self.scale, (i * 16 - 8) * self.scale,
                                             (j * 16 - 8) * self.scale, (i * 16 + 8) * self.scale,
-                                            fill=self.colors[self.points_map[i][j]],
+                                            fill=self.colors[self.map[i][j]],
                                             width=2 * self.scale)
 
     def to_scale(self, _scale):
@@ -82,12 +82,12 @@ class App:
 
     def translate(self, dx, dy):
         if self.position[1] <= -dy:
-            self.points_map.insert(0, [0] * len(self.points_map[0]))
+            self.map.insert(0, [-1] * len(self.map[0]))
             self.position = (self.position[0], self.position[1] + 1)
             self.origin = (self.origin[0], self.origin[1] + 1)
         if self.position[0] <= -dx:
-            for i in range(len(self.points_map)):
-                self.points_map[i].insert(0, 0)
+            for i in range(len(self.map)):
+                self.map[i].insert(0, -1)
             self.position = (self.position[0] + 1, self.position[1])
             self.origin = (self.origin[0] + 1, self.origin[1])
         self.canvas.delete('all')
@@ -95,8 +95,8 @@ class App:
 
     def create_point(self, x, y):
         x, y = x // (16 * self.scale) + self.position[0], y // (16 * self.scale) + self.position[1]
-        if self.points_map[y][x] == 0:
-            self.points_map[y][x] = int(self.is_green_turn) + 1
+        if self.map[y][x] == -1:
+            self.map[y][x] = int(self.is_green_turn)
             print(self.do_connect((x, y), (x, y)))
             self.canvas.delete('all')
             self.draw((self.position[0], self.position[1]))
