9ae07d4be473d983e8b87cf1d29914eb9a2f8183
NamorNiradnug <50048443+NamorNiradnug@users.noreply.github.com>
2019-10-06 18:34:38 +0300

    I finish this alrithm

--- client/dots.py
+++ client/dots.py
@@ -5,7 +5,7 @@ class Dots:
     def __init__(self):
         self.points = [[0] * 100 for __ in range(100)]
         self.colors = [0, '#2D2', 'blue']
-        self.all_tracks = []
+        self.tracks = {None}
 
         self.is_greens_turn = 1
         self.scale = 2
@@ -25,26 +25,37 @@ class Dots:
         self.root.bind('<Button-1>', lambda event: self.create_point(event.x, event.y))
         self.root.mainloop()
 
-    def do_connect(self, point1, point2, used_tracks=None, track_len=0):
-        if used_tracks is None:
-            used_tracks = {1}
-        result = False
-        print(point1, point2)
-        if point1 == point2 and track_len > 2:
-            print(3)
-            return True, used_tracks
-        for point in ((point1[0], point1[1] - 1), (point1[0] - 1, point1[1] - 1),
-                      (point1[0] - 1, point1[1]), (point1[0] + 1, point1[1] - 1),
-                      (point1[0] - 1, point1[1] + 1), (point1[0] + 1, point1[1]),
-                      (point1[0] + 1, point1[1] + 1), (point1[0], point1[1] + 1)):
-            if 0 < point[1] < len(self.points) and 0 < point[0] < len(self.points[point[1]]) and 0 < point1[1] < len(
-                    self.points) and 0 < point1[0] < len(self.points[point1[1]]) and self.points[point[1]][point[0]] ==\
-                    self.points[point1[1]][point1[0]] and not (((point,), point1) in used_tracks):
-                _next = self.do_connect((point,), point2, used_tracks and {((point,), point2), (point2, (point,))},
-                                        track_len + 1)
-                result += _next[0]
-                used_tracks.update(_next[1])
-        return bool(result), used_tracks
+    def do_connect(self, point):
+        print(0)
+        open_p = []
+        used = []
+        for i in ((point[0], point[1] - 1), (point[0] + 1, point[1] - 1),
+                  (point[0] + 1, point[1]),(point[0] + 1, point[1] + 1),
+                  (point[0], point[1] + 1),(point[0] - 1, point[1] + 1),
+                  (point[0] - 1, point[1]),(point[0] - 1, point[1] - 1)):
+            if self.points[i[1]][i[0]] == self.points[point[1]][point[0]]:
+                #print(i)
+                open_p += [(point,  i)]
+                print(open_p)
+        while len(open_p) > 0:
+            position = open_p[-1][1]
+            print(position)
+            used += open_p[-1:]
+            open_p.pop()
+            if position == point:
+                print(1)
+                self.tracks.update(set(used))
+                #used.clear()
+            else:
+                for i in ((position[0], position[1] - 1), (position[0] + 1, position[1] - 1),
+                          (position[0] + 1, position[1]),(position[0] + 1, position[1] + 1),
+                          (position[0], position[1] + 1),(position[0] - 1, position[1] + 1),
+                          (position[0] - 1, position[1]),(position[0] - 1, position[1] - 1)):
+                    if self.points[i[1]][i[0]] == self.points[position[1]][position[0]] and used.count((position, i)) == 0 and \
+                       used.count((i, position)) == 0 \
+                       and open_p.count((position, i)) == 0: #and open_p.count((i, position)) == 0:
+                        open_p += [(position,  i)]
+
 
     def draw_point(self, point_type, x, y):
         self.canvas.create_line((x * 16 + 8) * self.scale, y * self.scale * 16,
@@ -65,13 +76,13 @@ class Dots:
                 self.draw_point(self.points[y:y + h][i][x:x + w][j], j, i)
         for i in range(len(self.points[y:y + h])):
             for j in range(len(self.points[i][x:x + w])):
-                if 0 < y and y + h < len(self.points) and 0 < i < len(self.points[y:y + h]) and 0 < x and x + w < len(
-                        self.points[y:y + h][i]) and 0 < j < len(self.points[y:y + h][i][x:x + w]):
+                if 0 <= y and y + h < len(self.points) and 0 <= i < len(self.points[y:y + h]) and 0 <= x and x + w < len(
+                        self.points[y:y + h][i]) and 0 <= j < len(self.points[y:y + h][i][x:x + w]):
                     if j + 1 < len(self.points[y:y + h][i][x:x + w]) and self.points[y:y + h][i][x:x + w][j] == \
                             self.points[y:y + h][i][x:x + w][j + 1] > 0:
                         self.canvas.create_line((j * 16 + 8) * self.scale, (i * 16 + 8) * self.scale,
                                                 (j * 16 + 24) * self.scale, (i * 16 + 8) * self.scale,
-                                                fill=self.colors[self.points[y:y + h][i][x:x + w][j]],
+                                            fill=self.colors[self.points[y:y + h][i][x:x + w][j]],
                                                 width=2 * self.scale)
                     if i + 1 < len(self.points[y:y + h]) and self.points[y:y + h][i][x:x + w][j] == \
                             self.points[y:y + h][i + 1][x:x + w][j] > 0:
@@ -79,6 +90,14 @@ class Dots:
                                                 (j * 16 + 8) * self.scale, (i * 16 + 24) * self.scale,
                                                 fill=self.colors[self.points[y:y + h][i][x:x + w][j]],
                                                 width=2 * self.scale)
+        for i in self.tracks:
+            if i != None:
+                if x - 1 <= i[0][0] <= x + w + 1 and y - 1 <= i[0][1] <= y + h + 1 and \
+                   x - 1 <= i[1][0] <= x + w + 1 and y - 1 <= i[1][1] <= y + h + 1:
+                    self.canvas.create_line((i[0][0] * 16 + 8) * self.scale, (i[0][1] * 16 + 8) * self.scale, 
+                                            (i[1][0] * 16 + 8) * self.scale, (i[1][0] * 16 + 8) * self.scale,
+                                            fill=self.colors[self.points[i[0][1]][i[0][1]]], 
+                                            width=2 * self.scale)
 
     def set_scale(self, scale):
         if scale != 0 and scale != 16:
@@ -108,6 +127,8 @@ class Dots:
         x, y = x // (16 * self.scale) + self.position[0], y // (16 * self.scale) + self.position[1]
         if self.points[y][x] == 0:
             self.points[y][x] = self.is_greens_turn
+            self.do_connect((x, y))
+            print(self.tracks)
             self.canvas.delete('all')
             self.draw(self.position[0], self.position[1])
             if self.is_greens_turn == 1:
