17f2c9d409a4ef6a331046c4352d6a660cfd019f
NamorNiradnug <50048443+NamorNiradnug@users.noreply.github.com>
2019-12-06 19:29:07 +0300

    now i can  draw on window.canvas out of __init__

--- client/graphics.py
+++ client/graphics.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QLabel, QMainWindow, QApplication
 from PyQt5.QtGui import QPixmap, QPainter, QPen, QColor, QImage
-from PyQt5.QtCore import Qt, QLine
+from PyQt5.QtCore import Qt, QLine, QPoint
+from PIL import Image, ImageQt
 
 
 class Line(QLine):
@@ -20,19 +21,33 @@ class Line(QLine):
         painter.end()
 
 
+class Point(QPoint):
+    def __init__(self, x, y, **kwargs):
+        super().__init__(x, y)
+        self.radius = kwargs.get('rad', 1)
+        self.fill = kwargs.get('fill', 'black')
+                               
+    def draw(self, master):
+        painter = QPainter(master)
+        pen = QPen(QColor(self.fill))
+        painter.setPen(pen)
+        painter.drawPoint(self)
+        painter.end()
+        
 class DrawWindow(QMainWindow):
     def __init__(self, width=1980, height=1280):
         super().__init__()
         self.setWindowTitle('Dots')
         self.graphics_container = QLabel()
-        canvas = Canvas(width=width, height=height)
-        canvas.draw_obj(obj=Line(0, 0, 200, 200, fill='red', width=6), tag='line1')
-        canvas.draw_obj(x=200, y=200, obj=Canvas(width=200, height=300, color='black'), tag='thing')
-        canvas.draw_obj(obj=Line(0, 0, 20, 20, fill='white'), master_tag='thing')
+        self.setCentralWidget(self.graphics_container)
+        self.canvas = Canvas(width=width, height=height)
+        self.canvas.create_object(obj=Line(0, 0, 200, 200, fill='red', width=6), tag='line1')
+        self.canvas.create_object(obj=QImage(Data.image), x=0, y=0, tag='imge')
+        self.canvas.create_object(obj=QImage(Data.image), x=90, y=90, tag='pop')
         self.mouse_press_events = {}
         self.key_press_events = {}
-        self.graphics_container.setPixmap(canvas)
-        self.setCentralWidget(self.graphics_container)
+        self.canvas.create_object(obj=Line(0, 0, 90, 90, width=10, fill='blue'), tag='point')
+        self.graphics_update()
 
     def toggle_full_screen(self):
         if self.isFullScreen():
@@ -60,6 +75,9 @@ class DrawWindow(QMainWindow):
     def mouse_bind(self, event_button, x, y, function, arg=()):
         self.mouse_press_events[(event_button, x, y)] = (function, arg)
 
+    def graphics_update(self):
+        self.graphics_container.setPixmap(self.canvas)
+        self.update()
 
 class Canvas(QPixmap):
     def __init__(self, **kwargs):
@@ -70,71 +88,60 @@ class Canvas(QPixmap):
 
         self.color = kwargs.get('color', 'white')
         self.fill(QColor(self.color))
-        self.objects_tags = []
+        self.objects_tags = ['self']
         self.objects = {'self': self}
 
-    def create_image(self, x, y, image, tag):
-        painter = QPainter()
-        self.setPainter(painter)
-        painter.drawImage(x, y, image)
-        self.new_object(tag, image)
-        painter.end()
-
-    def create_canvas(self, x, y, canvas, tag):
-        painter = QPainter(self)
-        painter.drawPixmap(canvas)
-        self.new_object(tag, canvas)
-        painter.end()
-
-    def draw_line(self, line, tag, master_tag=None):
-        if master_tag == None:
-            master_tag = 'self'
-        painter = QPainter(self.objects_tags[master_tag])
-        pen = QPen()
-        pen.setColor(QColor(line.fill))
-        pen.setWidth(line.width)
-        painter.setPen(pen)
-        painter.drawLine(line)
-        self.new_object(tag, line, )
-        painter.end()
-
-    def create_object(self, obj, x, y, master, tag):
-        self.objects_tags.insert()
+    def create_object(self, **kwargs):
+        obj, x, y, master_tag, tag = kwargs['obj'], kwargs.get('x', None),\
+            kwargs.get('y', None), kwargs.get('master_tag', self.objects_tags[-1]), \
+            kwargs['tag']
+        self.objects_tags.insert(self.objects_tags.index(master_tag) + 1, tag)
         self.objects[tag] = obj, x, y
         self.update()
 
-    def draw_obj(self, **kwargs):
-        if 'obj' not in kwargs.keys():
+    def draw_obj(self, *args):
+        if type(args[0]) in {Line, Point}:
+            func = Data.draw_functions[type(args[0])]
+            print(type(args[0]))
+            func(args[0], self)
+            return
+
+        if len(args) != 3:
             raise AttributeError
-
-        x, y, obj = kwargs.get('x', 0), kwargs.get('y', 0), kwargs['obj']
-
-        if type(obj) == Line:
-            obj.draw(self)
-        else:
-            painter = QPainter(master)
-            func = Data.draw_functions[type(obj)]
-            func(painter, x, y, obj)
-            painter.end()
+        
+        obj, x, y = args
+        painter = QPainter(self)
+        func = Data.draw_functions[type(obj)]
+        func(painter, x, y, obj)
+        painter.end()
 
     def update(self):
         self.fill(QColor(self.color))
-        for tag in self.objects_tags:
-            self.draw_obj(self.objects[tag])
+        for tag in self.objects_tags[1:]:
+            self.draw_obj(*self.objects[tag])
 
+    def delete_object(self, obj_tag):
+        if obj_tag in self.objects_tags[1:]:
+            self.objects_tags.remove(obj_tag)
+            self.objects.pop(obj_tag)
+            self.update()
+        else:
+            raise KeyError
 
 class Data:
     draw_functions = {QPixmap: QPainter.drawPixmap, Canvas: QPainter.drawPixmap,
                       QLine: QPainter.drawLine, Line: Line.draw,
-                      QImage: QPainter.drawImage}
+                      QImage: QPainter.drawImage, Point: Point.draw}
+    image = ImageQt.ImageQt(Image.open('resources/settings.png'))
 
 
 app = QApplication(sys.argv)
 window = DrawWindow(900, 900)
-for i in range(800):
-    for j in range(800):
-        window.mouse_bind(1, i, j, print, (i, j))
-window.key_bind(65, print, ('Hello!', "You pressed 'A'."))
-
+# for i in range(800):
+#    for j in range(800):
+#        window.mouse_bind(1, i, j, print, (i, j))
+# window.key_bind(65, print, ('Hello!', "You pressed 'A'."))
+window.canvas.create_object(obj=Line(90, 90, 30, 200, width=5), tag='ninu')
+window.graphics_update()
 window.show()
 app.exec_()
