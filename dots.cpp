842c14308b79e5005c8c0f3abb53502918bca63a
NamorNiradnug <roma937a@mail.ru>
2020-05-05 23:47:42 +0300

    now it is game

--- dots.cpp
+++ dots.cpp
@@ -4,16 +4,14 @@
 #include <iostream>
 
 
-std::set<QPoint> circle(int cx, int cy, int radius = 1)
+std::set<QPoint> circle(int cx, int cy)
 {
-    std::set<QPoint> answer;
-    for (int x = -radius; x < radius + 1; x++)
-    {
-        for (int y = radius - std::abs(x); y < -radius + std::abs(x); y++)
-        {
-            answer.insert(QPoint(x + cx, y + cy));
-        }
-    }
+    std::set<QPoint> answer = {
+        QPoint(cx - 1, cy),
+        QPoint(cx + 1, cy),
+        QPoint(cx, cy - 1),
+        QPoint(cx, cy + 1)
+    };
     return answer;
 }
 
@@ -33,6 +31,30 @@ bool operator <(QLine line1, QLine line2)
         return line1.p1() < line2.p1();
     }
     return line1.p2() < line2.p2();
+}
+
+std::vector<QPoint> operator +(std::vector<QPoint> first, std::vector<QPoint> second)
+{
+    std::vector<QPoint> answer = first;
+    answer.insert(answer.end(), second.begin(), second.end());
+    return answer;
+}
+
+std::vector<QPoint> reversed(std::vector<QPoint> vec)
+{
+    std::reverse(vec.begin(), vec.end());
+    return vec;
+}
+
+int player(int dot)
+{
+    return dot % 3;
+}
+
+int eater(int dot)
+{
+    int e = dot / 3;
+    return e;
 }
 
 //#######################################
@@ -66,9 +88,27 @@ Chunk::Chunk(int x, int y, Settings *settings)
     this->y = y;
 }
 
+void Chunk::addTrack(QLine track)
+{
+    if (!tracks.count(QLine(track.p2(), track.p1())))
+    {
+        tracks.insert(track);
+    }
+}
+
+std::set<QLine> Chunk::getTracks()
+{
+    return tracks;
+}
+
 int Chunk::dot(int x, int y)
 {
     return map[x][y];
+}
+
+int Chunk::dot(QPoint pos)
+{
+    return map[pos.x()][pos.y()];
 }
 
 void Chunk::setDot(int x, int y, int dot)
@@ -76,22 +116,7 @@ void Chunk::setDot(int x, int y, int dot)
     map[x][y] = dot;
 }
 
-bool Chunk::isEmpty()
-{
-    for (auto line: map)
-    {
-        for (auto dot: line)
-        {
-            if (dot != 8)
-            {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-void Chunk::draw(QPainter *painter)
+void Chunk::drawLines(QPainter *painter)
 {
     QPen black_pen(Qt::black);
     QPen red_pen(Qt::red);
@@ -122,18 +147,83 @@ void Chunk::draw(QPainter *painter)
     {
         painter->drawLine(8 + (x + 15) * 16, y * 16, 8 + (x + 15) * 16, y * 16 + 256);
     }
+}
+
+void Chunk::drawDots(QPainter *painter, Dots *dots)
+{
+    for (QLine track: tracks)
+    {
+        painter->setPen(settings->colors()[player(dot(track.p1().x() % 16,
+                                                      track.p1().y() % 16))]);
+        painter->drawLine(track.p1() * 16 + QPoint(8, 8), track.p2() * 16 + QPoint(8, 8));
+    }
+    QBrush fill_brush = QBrush(Qt::transparent);
+    fill_brush.setStyle(Qt::BDiagPattern);
     painter->setPen(Qt::transparent);
     for (int x = 0; x < 16; x++)
     {
         for (int y = 0; y < 16; y++)
         {
-//            std::cout << x << " " << y  << " " << dot(x, y) << std::endl;
-//            if (dot(x, y) != 8)
-//            {
-//                std::cout << dot(x, y) << std::endl;
-//            }
-            painter->setBrush(settings->colors()[dot(x, y) % 3]);
+            painter->setBrush(settings->colors()[player(dot(x, y))]);
             painter->drawEllipse(dotCoord(x, y), 3, 3);
+            if (dot(x, y) == 8)
+            {
+                continue;
+            }
+            if (dots->isFirst(QPoint(x, y)))
+            {
+                painter->setBrush(Qt::transparent);
+                painter->setPen(settings->colors()[player(dot(x, y))]);
+                painter->drawEllipse(dotCoord(x, y), 6, 6);
+                painter->setPen(Qt::transparent);
+            }
+            fill_brush.setColor(settings->colors()[player(dot(x, y))]);
+            painter->setBrush(fill_brush);
+            std::vector<QPoint> circle_ = {
+                QPoint(x, y - 1),
+                QPoint(x + 1, y),
+                QPoint(x, y + 1),
+                QPoint(x - 1, y)
+            };
+            for (int i = 0; i < 4; i++)
+            {
+                if (dots->dot(circle_[i]) == dot(x, y) && dot(x, y) == dots->dot(circle_[(i + 1) % 4]))
+                {
+                    QPoint mono_triang[3] = {
+                        dotCoord(x, y),
+                        dotCoord(circle_[i]),
+                        dotCoord(circle_[(i + 1) % 4])
+                    };
+                    painter->drawPolygon(mono_triang, 3);
+                }
+            }
+            fill_brush.setColor(settings->colors()[eater(dot(x, y))]);
+            painter->setBrush(fill_brush);
+            QPoint poly[4] = {
+                dotCoord(x, y - 1),
+                dotCoord(x + 1, y),
+                dotCoord(x, y + 1),
+                dotCoord(x - 1, y)
+            };
+            painter->drawPolygon(poly, 4);
+            QPoint some_points[4] = {
+                QPoint(x - 1, y - 1),
+                QPoint(x - 1, y + 1),
+                QPoint(x + 1, y - 1),
+                QPoint(x + 1, y + 1)
+            };
+            for (QPoint pos: some_points)
+            {
+                if (player(dots->dot(pos.x() + this->x, pos.y() + this->y)) == eater(dot(x, y)))
+                {
+                    QPoint triang[3] = {
+                        dotCoord(pos),
+                        dotCoord(pos.x(), y),
+                        dotCoord(x, pos.y())
+                    };
+                    painter->drawPolygon(triang, 3);
+                }
+            }
         }
     }
 }
@@ -141,6 +231,11 @@ void Chunk::draw(QPainter *painter)
 QPoint Chunk::dotCoord(int x, int y)
 {
     return QPoint((this->x + x) * 16 + 8, (this->y + y) * 16 + 8);
+}
+
+QPoint Chunk::dotCoord(QPoint pos)
+{
+    return dotCoord(pos.x(), pos.y());
 }
 
 //#######################################
@@ -159,9 +254,25 @@ Dots::Dots()
 
 void Dots::addTrack(std::vector<QPoint> track)
 {
+    QPoint max_dot, min_dot;
     for (unsigned i = 0; i < track.size() - 1; i++)
     {
-        tracks.insert(QLine(track[i], track[i + 1]));
+        max_dot = track[i];
+        min_dot = track[i + 1];
+        if (max_dot < min_dot)
+        {
+            std::swap(min_dot, max_dot);
+        }
+        if (max_dot.x() != min_dot.x() and max_dot.y() != min_dot.y())
+        {
+            QLine intersec = QLine(max_dot.x(), min_dot.y(), min_dot.x(), max_dot.y());
+            if (chunk(intersec.x1(), intersec.y1())->getTracks().count(intersec))
+            {
+                continue;
+            }
+        }
+        chunk(max_dot.x(), max_dot.y())->addTrack(QLine(min_dot, max_dot));
+        chunk(min_dot.x(), min_dot.y())->addTrack(QLine(min_dot, max_dot));
     }
 }
 
@@ -172,7 +283,7 @@ std::set<QPoint> Dots::surrounding(int x, int y)
     {
         for (int dy: {-1, 0, 1})
         {
-            if (not (dx == 0 and dy == 0) and this->dot(x + dx, y + dy) == this->dot(x, y))
+            if (!(dx == 0 and dy == 0) and dot(x + dx, y + dy) == dot(x, y))
             {
                 answer.insert(QPoint(x + dx, y + dy));
             }
@@ -189,6 +300,11 @@ double Dots::getScale()
 QPoint Dots::camPos()
 {
     return QPoint(cam_x, cam_y);
+}
+
+bool Dots::isFirst(QPoint pos)
+{
+    return pos == first1 or pos == second1;
 }
 
 void Dots::changeScale(double delta, QSize size)
@@ -211,6 +327,11 @@ int Dots::dot(int x, int y)
     return chunk(x, y)->dot(x % 16, y % 16);
 }
 
+int Dots::dot(QPoint pos)
+{
+    return dot(pos.x(), pos.y());
+}
+
 void Dots::translate(QPoint delta, QSize size)
 {
     size /= scale;
@@ -227,15 +348,117 @@ void Dots::normalize(QSize size)
     cam_y = std::min(cam_y, 4096 - size.height() / 2);
 }
 
-void Dots::findNewTracksAndEaten(int x, int y){}
+void Dots::findNewTracksAndEaten(int x, int y)
+{
+    //-----find new tracks-----
+    std::set<std::vector<QPoint>> open_tracks = {{QPoint(x, y)}}, open_tracks_copy;
+    std::set<QPoint> used = {QPoint(x, y)};
+    std::vector<QPoint> new_track;
+    while (!open_tracks.empty())
+    {
+        open_tracks_copy = open_tracks;
+        open_tracks = {};
+        for (std::vector<QPoint> track: open_tracks_copy)
+        {
+            for (QPoint pos: surrounding(track.back().x(), track.back().y()))
+            {
+                if (!used.count(pos))
+                {
+                    new_track = track;
+                    new_track.push_back(pos);
+                    open_tracks.insert(new_track);
+                    used.insert(pos);
+                }
+                else if (track.size() >= 3 and pos == QPoint(x, y))
+                {
+                    addTrack(track + std::vector<QPoint>(1, pos));
+                }
+                else if (pos != QPoint(x, y))
+                {
+                    for (std::vector<QPoint> other: open_tracks_copy)
+                    {
+                        if (other[1] != track[1] and pos == other.back())
+                        {
+                            addTrack(track + reversed(other));
+                        }
+                    }
+                    for (std::vector<QPoint> other: open_tracks)
+                    {
+                        if (other[1] != track[1] and pos == other.back())
+                        {
+                            addTrack(track + reversed(other));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //------find new eaten-----
+    std::set<QPoint> open, open_copy;
+    bool not_eaten;
+    for (QPoint start: circle(x, y))
+    {
+        used = {};
+        open = {start};
+        not_eaten = false;
+        while (!open.empty() and !not_eaten)
+        {
+            open_copy = open;
+            open = {};
+            for (QPoint pos: open_copy)
+            {
+                if (!QRect(min_x, min_y, max_x - min_x, max_y - min_y).contains(pos))
+                {
+                    not_eaten = true;
+                    break;
+                }
+                for (QPoint p: circle(pos.x(), pos.y()))
+                {
+                    if (!used.count(p) and !open.count(p) and dot(p.x(), p.y()) != dot(x, y))
+                    {
+                        open.insert(p);
+                    }
+                }
+                used.insert(pos);
+            }
+        }
+        if (!not_eaten)
+        {
+            for (QPoint pos: used)
+            {
+                chunk(pos.x(), pos.y())->setDot(pos.x() % 16, pos.y() % 16,
+                                                player(dot(pos.x(), pos.y())) + 3 * player(dot(x, y)));
+            }
+        }
+    }
+}
 
 void Dots::turn(QPoint pos)
 {
     if (QRect(1, 1, 255, 255).contains(pos.x(), pos.y()) and dot(pos.x(), pos.y()) == 8)
     {
-        chunk(pos.x(), pos.y())->setDot(pos.x() % 16, pos.y() % 16, turning_player);
-        std::cout << pos.x() << " " << pos.y() << std::endl;
+        chunk(pos.x(), pos.y())->setDot(pos.x() % 16, pos.y() % 16, turning_player + 6);
+        max_x = std::max(max_x, pos.x());
+        max_y = std::max(max_y, pos.y());
+        min_x = std::min(min_x, pos.x());
+        min_y = std::min(min_y, pos.y());
         this->findNewTracksAndEaten(pos.x(), pos.y());
+        if (first1 == QPoint(-1, -1) and turning_player == 0)
+        {
+            first1 = pos;
+        }
+        if (second1 == QPoint(-1, -1) and turning_player == 1)
+        {
+            second1 = pos;
+        }
+        if (first1 != QPoint(-1, -1) and eater(dot(first1)) == 1)
+        {
+            std::cout << "Second player won!\n";
+        }
+        if (second1 != QPoint(-1, -1) and eater(dot(second1)) == 0)
+        {
+            std::cout << "First player won!\n";
+        }
         turning_player = (turning_player + 1) % 2;
     }
 }
@@ -247,17 +470,24 @@ void Dots::draw(QSize size, QPainter *painter, QPoint cursor)
     normalize(size);
     int tx = size.width() / 2 - cam_x, ty = size.height() / 2 - cam_y;
     painter->translate(tx, ty);
+    std::set<Chunk*> visible = {};
     for (int x = 0; x < 256; x += 16)
     {
         for (int y = 0; y < 256; y += 16)
         {
-            if (-256 < x * 16 + tx && x * 16 + tx <= size.width() and
-                -256 < y * 16 + ty && y * 16 + ty <= size.height())
+            if (-256 < x * 16 + tx and x * 16 + tx <= size.width() and
+                -256 < y * 16 + ty and y * 16 + ty <= size.height())
             {
-                chunk(x, y)->draw(painter);
+                visible.insert(chunk(x, y));
+                chunk(x, y)->drawLines(painter);
             }
         }
     }
+    for (Chunk *chunk: visible)
+    {
+        chunk->drawDots(painter, this);
+    }
+    painter->setPen(Qt::transparent);
     if (QRect(1, 1, 255, 255).contains(cursor) and
             dot(cursor.x(), cursor.y()) == 8)
     {
--- dots.h
+++ dots.h
@@ -11,7 +11,11 @@ class Dots;
 
 bool operator <(QPoint point1, QPoint point2);
 bool operator <(QLine line1, QLine line2);
-
+std::vector<QPoint> operator +(std::vector<QPoint> first, std::vector<QPoint> second);
+std::vector<QPoint> reversed(std::vector<QPoint> vec);
+QPoint max(QPoint p1, QPoint p2);
+int player(int dot);
+int eater(int dot);
 
 class Settings
 {
@@ -26,14 +30,19 @@ class Chunk
 {
 public:
     Chunk(int x, int y, Settings *settings);
+    void addTrack(QLine track);
+    std::set<QLine> getTracks();
     int dot(int x, int y);
+    int dot(QPoint pos);
     void setDot(int x, int y, int dot);
-    bool isEmpty();
-    void draw(QPainter *painter);
+    void drawLines(QPainter *painter);
+    void drawDots(QPainter *painter, Dots* dots);
 private:
     QPoint dotCoord(int x, int y);
+    QPoint dotCoord(QPoint pos);
     int x, y;
     std::vector<std::vector<int>> map;
+    std::set<QLine> tracks;
     Settings *settings;
 };
 
@@ -47,8 +56,10 @@ public:
     Chunk* chunk(int x, int y);
     void draw(QSize size, QPainter *painter, QPoint cursor);
     int dot(int x, int y);
+    int dot(QPoint pos);
     double getScale();
     QPoint camPos();
+    bool isFirst(QPoint pos);
     void findNewTracksAndEaten(int x, int y);
     void normalize(QSize size);
     std::set<QPoint> surrounding(int x, int y);
@@ -56,8 +67,10 @@ public:
     void turn(QPoint pos);
 private:
     int cam_x = 0, cam_y = 0, turning_player = 0;
-    std::set<QLine> tracks;
+    int min_x = 256, max_x = 256, min_y = 0, max_y = 0;
     double scale = 2.0;
     std::vector<std::vector<Chunk>> chunks;
     Settings *settings = new Settings();
+    QPoint first1 = QPoint(-1, -1), second1 = QPoint(-1, -1); // the first dot of players
+
 };
